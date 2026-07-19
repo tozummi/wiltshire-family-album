0484c1cae8d441dc087582451f4453c2c504be85
tozummi <tozummi@gmail.com>
2026-07-19 22:38:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function checkPin() {
 
+  const pin = document.getElementById("pin").value;
+
+  if (pin === "WILTSHIRE26") {
+
+    document.getElementById("login-box").style.display = "none";
+    document.getElementById("album").style.display = "block";
+
+  } else {
+
+    alert("Incorrect family code");
+
+  }
+
+}
