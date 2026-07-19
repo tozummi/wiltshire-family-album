2dcddc1be985f9313a94a37231bcdd653b323b59
tozummi <tozummi@gmail.com>
2026-07-19 23:51:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function checkPin() {
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 
-  const pin = document.getElementById("pin").value;
+async function checkPin() {
 
-  if (pin === "WILTSHIRE26") {
+  const enteredPin = document.getElementById("pin").value;
+
+  const { data, error } = await supabaseClient
+    .from("settings")
+    .select("value")
+    .eq("key", "album_pin")
+    .single();
+
+  if (error) {
+    console.log(error);
+    alert("Something went wrong");
+    return;
+  }
+
+  if (enteredPin === data.value) {
 
     document.getElementById("login-box").style.display = "none";
     document.getElementById("album").style.display = "block";
