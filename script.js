075af70e0020a5910f3d4651d41ca1ea100f0a24
tozummi <tozummi@gmail.com>
2026-07-21 12:01:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -543,6 +543,7 @@ async function loadCurrentReaction() {
     .from("photo_reactions")
     .select("reaction")
     .eq("photo_id", currentPhotoId)
+    .eq("user_id", currentUser.id)
     .maybeSingle();
 
   console.log("Checking reaction for:", {
