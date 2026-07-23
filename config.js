afebba9212d6eb69ec3d13e66c6545553c934a5c
tozummi <tozummi@gmail.com>
2026-07-23 01:19:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,20 @@
-const SUPABASE_URL = "https://qoeiqvoaqqfheojaanad.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZWlxdm9hcXFmaGVvamFhbmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0ODMzODEsImV4cCI6MjEwMDA1OTM4MX0.fAkhkc2m7VpVo5Z59LSAJK-_No0xNnt6eLX3U4oSPvg";
+const SUPABASE_URL =
+    "https://qoeiqvoaqqfheojaanad.supabase.co";
+
+const SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZWlxdm9hcXFmaGVvamFhbmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0ODMzODEsImV4cCI6MjEwMDA1OTM4MX0.fAkhkc2m7VpVo5Z59LSAJK-_No0xNnt6eLX3U4oSPvg";
+
+const VIDEO_CLOUD_NAME =
+    "xm08ovod";
+
+const VIDEO_UPLOAD_PRESET =
+    "family_album_videos";
+
+const MAX_VIDEO_SIZE_MB =
+    50;
+
+const MAX_VIDEO_SIZE_BYTES =
+    MAX_VIDEO_SIZE_MB * 1024 * 1024;
+
+const MAX_VIDEO_DURATION_SECONDS =
+    5 * 60;
